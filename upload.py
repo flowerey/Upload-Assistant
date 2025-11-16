7c156edb83443904f29d874f0626ab4d7d4ceade
flowerey <blu3berry@disroot.org>
2025-11-16 17:53:23 +0300

    actually update mkbrr

--- upload.py
+++ upload.py
@@ -1089,7 +1089,7 @@ async def do_the_thing(base_dir):
 
 async def get_mkbrr_path(meta, base_dir=None):
     try:
-        mkbrr_path = await ensure_mkbrr_binary(base_dir, debug=meta['debug'], version="v1.14.0")
+        mkbrr_path = await ensure_mkbrr_binary(base_dir, debug=meta['debug'], version="v1.18.0")
         return mkbrr_path
     except Exception as e:
         console.print(f"[red]Error setting up mkbrr binary: {e}[/red]")
